54e30be53c266d345d86d072d7f04f847a871f5a
peak <peaktai@qq.com>
2022-06-29 19:52:29 +0800

    去除代码的复制功能，用处不大，还容易挡代码，影响移动端体验

--- src/components/CodeHighlight.vue
+++ src/components/CodeHighlight.vue
@@ -1,55 +1,8 @@
 <template>
-  <div class="position-relative">
-    <template v-if="data.clipboardVisible">
-      <div
-        v-if="!data.copied"
-        class="position-absolute lh-1"
-        style="cursor: pointer; top: 0.3em; right: 0.3em"
-        @click="copy"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          style="height: 1em"
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
-          />
-          <path
-            d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
-          />
-        </svg>
-      </div>
-      <div
-        v-if="data.copied"
-        class="position-absolute text-success lh-1"
-        style="cursor: pointer; top: 0.3em; right: 0.3em"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          style="height: 1em"
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"
-          />
-          <path
-            d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
-          />
-          <path
-            d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
-          />
-        </svg>
-      </div>
-    </template>
-    <pre
-      class="bg-light p-3 lh-base"
-      :class="{ 'pre-line': !!props.autoWrap }"
-    ><code :class="[data.className]" v-html="data.highlightCode"></code></pre>
-  </div>
+  <pre
+    class="bg-light p-3 lh-base"
+    :class="{ 'pre-line': !!props.autoWrap }"
+  ><code :class="[data.className]" v-html="data.highlightCode"></code></pre>
 </template>
 <script lang="ts" setup>
 import hljs from 'highlight.js'
@@ -72,9 +25,7 @@ const props = defineProps({
 
 const data = reactive({
   highlightCode: '',
-  className: '',
-  copied: false,
-  clipboardVisible: navigator && navigator.clipboard
+  className: ''
 })
 
 buildCodeStyle()
@@ -98,7 +49,7 @@ function buildCodeStyle() {
   // 实例得到的值是 '\n    default boolean isEmpty() ...'
   // 将编写代码时增加的缩进给去除下
   let finalCode = props.code
-  const lines = props.code.split(/\r|\n/).filter(line => !!line)
+  const lines = props.code.split(/\r?\n/).filter(line => !!line)
   if (lines.length) {
     let indent = 99999
     lines.forEach(line => {
@@ -132,18 +83,6 @@ function spaceCountAtBeginning(str: string) {
     }
   }
   return count
-}
-
-function copy() {
-  navigator.clipboard
-    .writeText(props.code)
-    .then(() => {
-      data.copied = true
-      setTimeout(() => (data.copied = false), 2000)
-    })
-    .catch(error => {
-      console.error(`复制代码失败`, error)
-    })
 }
 </script>
 <style>
--- src/components/MarkdownPage.vue
+++ src/components/MarkdownPage.vue
@@ -42,68 +42,6 @@ onMounted(() => {
   // 代码
   content.value.querySelectorAll('pre').forEach(pre => {
     pre.classList.add('bg-light', 'p-3', 'lh-base')
-    const wrapper = document.createElement('div')
-    wrapper.classList.add('position-relative', 'd-none', 'd-md-block')
-    const copyBtn = document.createElement('div')
-    copyBtn.className = 'position-absolute lh-1'
-    const copySvg = `
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            style="height: 1em"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-          <path
-              d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
-            />
-            <path
-              d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
-            />
-          </svg>`
-    const copiedSvg = `
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            style="height: 1em;"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"
-            />
-          </svg>`
-    copyBtn.innerHTML = copySvg
-    copyBtn.style.cursor = 'pointer'
-    copyBtn.style.top = '0.3em'
-    copyBtn.style.right = '0.3em'
-    wrapper.appendChild(copyBtn)
-    copyBtn.addEventListener('click', () => {
-      if (!navigator.clipboard) {
-        return
-      }
-      if (copyBtn.getAttribute('copied')) {
-        return
-      }
-      navigator.clipboard
-        .writeText(pre.innerText)
-        .then(() => {
-          copyBtn.classList.add('text-success')
-          copyBtn.innerHTML = copiedSvg
-          copyBtn.setAttribute('copied', 'true')
-          setTimeout(() => {
-            copyBtn.classList.remove('text-success')
-            copyBtn.innerHTML = copySvg
-            copyBtn.removeAttribute('copied')
-          }, 2000)
-        })
-        .catch(error => {
-          console.error(`复制代码失败`, error)
-        })
-    })
-
-    if (pre.parentNode) {
-      pre.parentNode.insertBefore(wrapper, pre)
-      wrapper.appendChild(pre)
-    }
   })
   // 图片
   content.value.querySelectorAll('img').forEach(img => {
